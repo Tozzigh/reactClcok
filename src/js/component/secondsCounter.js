5ab0a8f2ac20c35899ee1709c5225389b553df7c
Tozzigh <tozzigh@gmail.com>
2020-06-01 13:30:54 +0000

    v2

--- src/js/component/secondsCounter.js
+++ src/js/component/secondsCounter.js
@@ -1,42 +1,67 @@
 import React, { useState, useEffect } from "react";
 
-const SecondsCounter = () => {
-	var [clock, setClock] = useState("0000");
-	var [clockStr, setClockStr] = useState([]);
+function SecondsCounter() {
+	var [clock, setClock] = useState(58);
+	var [stopResume, setStopResume] = useState(false);
+
 	useEffect(() => {
-		setInterval(() => {
-			setClock(parseInt(clock++));
-			setClockStr(clock.toString().split(""));
+		const count = setInterval(() => {
+			if (
+				clock.toString()[clock.toString().length - 1] === "0" &&
+				clock.toString()[clock.toString().length - 2] === "6"
+			) {
+				setClock(clock => clock + 40);
+			} else {
+				setClock(clock => clock + 1);
+			}
 		}, 1000);
 	}, []);
-
+	useEffect(
+		() => {
+			console.log(
+				clock.toString()[clock.toString().length - 1] === "0" &&
+					clock.toString()[clock.toString().length - 2] === "6"
+			);
+		},
+		[clock]
+	);
 	return (
 		<div className="clock">
 			<div className="container-fluid">
 				<div className="ciao fas fa-hourglass-half" />
-				<div className="ciao">{clockStr[clockStr.length - 6]}</div>
-				<div className="ciao">{clockStr[clockStr.length - 5]}</div>
+				<div className="ciao">
+					{clock.toString()[clock.toString().length - 6] || 0}
+				</div>
+				<div className="ciao">
+					{clock.toString()[clock.toString().length - 5] || 0}
+				</div>
 				<div className="due">:</div>
-				<div className="ciao">{clockStr[clockStr.length - 4]}</div>
-				<div className="ciao">{clockStr[clockStr.length - 3]}</div>
+				<div className="ciao">
+					{clock.toString()[clock.toString().length - 4] || 0}
+				</div>
+				<div className="ciao">
+					{clock.toString()[clock.toString().length - 3] || 0}
+				</div>
 				<div className="due">:</div>
-				<div className="ciao">{clockStr[clockStr.length - 2]}</div>
-				<div className="ciao">{clockStr[clockStr.length - 1]}</div>
+				<div className="ciao">
+					{clock.toString()[clock.toString().length - 2] || 0}
+				</div>
+				<div className="ciao">
+					{clock.toString()[clock.toString().length - 1] || 0}
+				</div>
 			</div>
 			<input
 				type="number"
 				className="countdown"
 				placeholder="If u need a countdown, type here the start number"
-				onKeyUp={countdown}
+				onKeyPress={countdown}
 			/>
+			<button onClick={() => setStopResume(true)} />
 		</div>
 	);
-};
+}
 const countdown = evt => {
-	if (
-		(evt.which != 8 && evt.which != 0 && evt.which < 48) ||
-		evt.which > 57
-	) {
+	if (evt.key === "e" || evt.key === "+" || evt.key === "-") {
 		evt.preventDefault();
 	}
 };
