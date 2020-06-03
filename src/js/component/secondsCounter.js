ed338d99ce4d023007ba1040c58983d915daad47
Tozzigh <tozzigh@gmail.com>
2020-06-03 14:33:31 +0000

    Need to implement countdown function

--- src/js/component/secondsCounter.js
+++ src/js/component/secondsCounter.js
@@ -1,53 +1,71 @@
 import React, { useState, useEffect } from "react";
 
 function SecondsCounter() {
-	var [clock, setClock] = useState(58);
-	var [stopResume, setStopResume] = useState(false);
-
-	useEffect(() => {
-		const count = setInterval(() => {
-			if (
-				clock.toString()[clock.toString().length - 1] === "0" &&
-				clock.toString()[clock.toString().length - 2] === "6"
-			) {
-				setClock(clock => clock + 40);
-			} else {
-				setClock(clock => clock + 1);
-			}
-		}, 1000);
-	}, []);
+	var [seconds, setSeconds] = useState(0);
+	var [minutes, setMinutes] = useState(0);
+	var [hours, setHours] = useState(0);
+	var [pauseStart, setPauseStart] = useState(false);
 	useEffect(
 		() => {
-			console.log(
-				clock.toString()[clock.toString().length - 1] === "0" &&
-					clock.toString()[clock.toString().length - 2] === "6"
-			);
+			if (pauseStart) {
+				const id = setInterval(() => {
+					setSeconds(seconds => {
+						if (seconds < 60) {
+							return seconds + 1;
+						}
+					});
+				}, 1000);
+				return () => clearInterval(id);
+			}
 		},
-		[clock]
+		[pauseStart]
+	);
+
+	useEffect(
+		() => {
+			if (seconds == 60) {
+				setSeconds(seconds => (seconds = 0));
+				setMinutes(minutes => {
+					return minutes + 1;
+				});
+			}
+		},
+		[seconds]
+	);
+	useEffect(
+		() => {
+			if (minutes == 60) {
+				setHours(hours => {
+					setMinutes(minutes => (minutes = 0));
+					return hours + 1;
+				});
+			}
+		},
+		[minutes]
 	);
 	return (
 		<div className="clock">
 			<div className="container-fluid">
 				<div className="ciao fas fa-hourglass-half" />
 				<div className="ciao">
-					{clock.toString()[clock.toString().length - 6] || 0}
+					{hours.toString()[hours.toString().length - 2] || 0}
 				</div>
 				<div className="ciao">
-					{clock.toString()[clock.toString().length - 5] || 0}
-				</div>
-				<div className="due">:</div>
-				<div className="ciao">
-					{clock.toString()[clock.toString().length - 4] || 0}
-				</div>
-				<div className="ciao">
-					{clock.toString()[clock.toString().length - 3] || 0}
+					{hours.toString()[hours.toString().length - 1] || 0}
 				</div>
 				<div className="due">:</div>
 				<div className="ciao">
-					{clock.toString()[clock.toString().length - 2] || 0}
+					{minutes.toString()[minutes.toString().length - 2] || 0}
 				</div>
 				<div className="ciao">
-					{clock.toString()[clock.toString().length - 1] || 0}
+					{minutes.toString()[minutes.toString().length - 1] || 0}
+				</div>
+				<div className="due">:</div>
+				<div className="ciao">
+					{seconds.toString()[seconds.toString().length - 2] || 0}
+				</div>
+				<div className="ciao">
+					{seconds.toString()[seconds.toString().length - 1] || 0}
 				</div>
 			</div>
 			<input
@@ -56,7 +74,25 @@ function SecondsCounter() {
 				placeholder="If u need a countdown, type here the start number"
 				onKeyPress={countdown}
 			/>
-			<button onClick={() => setStopResume(true)} />
+			{pauseStart ? (
+				<button
+					className="fa fa-pause"
+					onClick={() => setPauseStart((pauseStart = false))}
+				/>
+			) : (
+				<button
+					className="fa fa-play"
+					onClick={() => setPauseStart((pauseStart = true))}
+				/>
+			)}
+			<button
+				className="fa fa-redo"
+				onClick={() => {
+					setSeconds(seconds => (seconds = 0));
+					setMinutes(minutes => (minutes = 0));
+					setHours(hours => (hours = 0));
+				}}
+			/>
 		</div>
 	);
 }
